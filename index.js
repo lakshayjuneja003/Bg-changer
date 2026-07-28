cb634636dfc1f9ad48839e37a42c90773e457e18
lakshayjuneja <lakshayjuneja003@gmail.com>
2026-07-28 19:10:45 +0530

    project overview

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 console.log("hello toodler")
+// Explanation : you have to create a number guessing system , in which system generate a random number btw 1 to 100 , and user has only 10 attempts to guess that number , in each guess you have to print
+//  the user guess
+// either too low or too high 
+// decrment the no of attempts user have 
+// disable the submit btn , once all attempts are exhausted
+
+function validateGuess(guess) {
+    // Check if the guess is a valid number
+}
+function checkGuess(guess) {
+    // Check if the guess is correct
+    // if not , tell the user if the guess is too high or too low
+}
+
+function displayGuess(guess) {
+    // Display the guess on the page
+    // Clear the input field
+    // Increment the number of guesses
+    // Update the remaining guesses
+}
+
+function displayMessage(message){
+    // Display a message to the user
+}
+
+function endGame(){
+    // Disable the input field and submit button
+    // end the game and start new one
+}
+
+function newGame(){
+    // start new game
+    // clear all input fields
+    
+}
